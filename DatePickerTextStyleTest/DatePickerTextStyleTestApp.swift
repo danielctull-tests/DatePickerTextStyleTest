d56a8aecbc30670f73454e42dccc4b65b6ae11f8
Daniel Tull <dt@danieltull.co.uk>
2022-07-06 11:56:39 +0100

    Initial Commit

---
+++ DatePickerTextStyleTest/DatePickerTextStyleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerTextStyleTestApp.swift
+//  DatePickerTextStyleTest
+//
+//  Created by Daniel Tull on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerTextStyleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
