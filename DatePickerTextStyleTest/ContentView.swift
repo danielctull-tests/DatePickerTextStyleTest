a109fcb01ee69285c192a5a23e6162f8edc34df9
Daniel Tull <dt@danieltull.co.uk>
2022-07-06 12:14:09 +0100

    Use a custom date picker style to style the components as we wish

--- DatePickerTextStyleTest/ContentView.swift
+++ DatePickerTextStyleTest/ContentView.swift
@@ -1,25 +1,49 @@
-//
-//  ContentView.swift
-//  DatePickerTextStyleTest
-//
-//  Created by Daniel Tull on 06/07/2022.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var date: Date = .now
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            DatePicker(selection: $date) {
+                Text("Date")
+            }
         }
+        .datePickerStyle(MyDatePickerStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MyDatePickerStyle: DatePickerStyle {
+
+    @State private var showPicker = false
+
+    func makeBody(configuration: Configuration) -> some View {
+
+        HStack {
+            configuration.label
+            Button {
+                showPicker.toggle()
+            } label: {
+                Text(configuration.selection.formatted(date: .long, time: .omitted))
+                    .bold()
+                    .padding(6)
+                    .background {
+                        RoundedRectangle(cornerRadius: 6).fill(.orange)
+                    }
+                    .foregroundColor(.white)
+            }
+            .sheet(isPresented: $showPicker) {
+                DatePicker(selection: configuration.$selection, displayedComponents: .date, label: EmptyView.init)
+                    .datePickerStyle(.graphical)
+
+            }
+        }
     }
 }
